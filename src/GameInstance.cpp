6dda09323889f30ab25cc865fd9e1d0bbf1c3f53
Caleb Bradley <dev.pebble225@gmail.com>
2021-12-09 09:51:39 -0600

    Triangle on screen.

--- src/GameInstance.cpp
+++ src/GameInstance.cpp
@@ -26,7 +26,16 @@ GameInstance::GameInstance()
 
 void GameInstance::init()
 {
-	
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(testingMesh::vertices), testingMesh::vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glDisableVertexAttribArray(0);
 }
 
 void GameInstance::Update(uint64_t runtime)
@@ -36,7 +45,11 @@ void GameInstance::Update(uint64_t runtime)
 
 void GameInstance::Render()
 {
-	
+	glEnableVertexAttribArray(0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(0);
 }
 
 GameInstance::~GameInstance()
